b35a34c582343863ec3272d03e8ba45c85bfe667
vivekpatel4321 <vivek.patel@tecblic.com>
2026-02-05 11:44:50 +0300

    martketplace 2.5 improvements

--- app/public/wp-content/themes/houzez-child/functions.php
+++ app/public/wp-content/themes/houzez-child/functions.php
@@ -49,7 +49,7 @@ add_filter('wp_mail', function ($args) {
  * Change "Send Message" button text to "Send Email" on property forms
  * This filters the houzez_option function output by modifying the global options array
  */
-add_action('wp_loaded', function() {
+add_action('init', function() {
     global $houzez_options;
     
     // Ensure the global options array exists
@@ -76,4 +76,86 @@ add_filter('redux/options/houzez_options/spl_btn_send', function($value) {
 add_filter('redux/options/houzez_options/spl_btn_message', function($value) {
     return ($value === 'Send Message') ? 'Send Email' : $value;
 }, 20);
-?>
+
+
+/**
+ * Change the Microsoft login button color to the same as the theme color on login screen
+ */
+add_action('login_enqueue_scripts', function () {
+    ?>
+    <style>
+        /* miniOrange Azure login button - default */
+        body.login .moazure_login_button {
+            background-color: #005AA9 !important;
+            border-color: #005AA9 !important;
+            color: #ffffff !important;
+        }
+
+        /* hover state */
+        body.login .moazure_login_button:hover {
+            background-color: #F08121 !important;
+            border-color: #F08121 !important;
+            color: #ffffff !important;
+        }
+    </style>
+    <?php
+});
+
+/**
+ * Change the Houzez Country taxonomy labels to 'Counties'
+ */
+
+add_action('init', function () {
+
+    // Houzez Country taxonomy slug
+    if (!taxonomy_exists('property_country')) {
+        return;
+    }
+
+    global $wp_taxonomies;
+
+    $labels = &$wp_taxonomies['property_country']->labels;
+
+    // Plural
+    $labels->name = 'Counties';
+    $labels->all_items = 'All Counties';
+    $labels->search_items = 'Search Counties';
+    $labels->popular_items = 'Popular Counties';
+
+    // Singular
+    $labels->singular_name = 'County';
+    $labels->edit_item = 'Edit County';
+    $labels->update_item = 'Update County';
+    $labels->add_new_item = 'Add New County';
+    $labels->new_item_name = 'New County Name';
+
+    // Menu
+    $labels->menu_name = 'Counties';
+});
+
+add_filter('houzez_search_country_label', function () {
+    return 'Counties';
+});
+
+add_filter('houzez_search_country_placeholder', function () {
+    return 'All Counties';
+});
+
+add_filter('houzez_country_terms_args', function ($args) {
+    $args['hide_empty'] = false;
+    return $args;
+});
+
+// Ensure state/city/country dropdowns show empty terms (don't hide empty)
+add_action('init', function() {
+    global $houzez_options;
+
+    if (!isset($houzez_options)) {
+        $houzez_options = get_option('houzez_options', array());
+    }
+
+    // Set to 0 (disabled) so houzez_hide_empty_taxonomies() returns false
+    $houzez_options['state_city_area_dropdowns'] = 0;
+    // Set search placeholder label to 'All Counties'
+    $houzez_options['srh_countries'] = 'All Counties';
+}, 100);
